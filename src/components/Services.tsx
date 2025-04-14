060c222388a08c0417ab17f232ef2db4025d5317
FarizW <m.fariz@k3mart.id>
2025-04-14 20:39:59 +0700

    feat: link github

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { link } from "fs";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useRef } from "react";
@@ -12,6 +13,7 @@ const Services = () => {
       link: "http://3d-soda-can-gilt.vercel.app/",
       image: "/assets/images/soda-can.gif",
       desc: "3D website with moving 3D assets of the products. I made this using Typescript, Next.JS, Tailwind CSS, Prismic, Three.JS, Figma and GSAP",
+      linkGithub: "https://github.com/farizwrmn/3d-soda-can",
     },
     {
       id: 2,
@@ -19,6 +21,7 @@ const Services = () => {
       link: "https://event-management-platform-jcwdol013-01.vercel.app",
       image: "/assets/images/minpro.gif",
       desc: "Event Management Website made with Typescript, Next.js, Tailwind CSS, Clerk, Uploadthing and MongoDB.",
+      linkGithub: "https://github.com/farizwrmn/event-management-platform-jcwdol013-01",
     },
     {
       id: 3,
@@ -26,6 +29,7 @@ const Services = () => {
       link: "https://jcwdol130201.purwadhikabootcamp.com",
       image: "/assets/images/e-commerce.gif",
       desc: "E-Groceries for Final Project at Purwadhika Bootcamp made with Typescript, Next.js, Chakra UI, Multer, Nodemailer, Formik and Yup, Axios, React Slick, Chart.js, MySQL, JWT, Express, Xendit, Rajaongkir API and Prisma. ",
+      linkGithub: "https://github.com/farizwrmn/finpro-jcwdol-013-01",
     },
     {
       id: 4,
@@ -33,6 +37,7 @@ const Services = () => {
       link: "https://company-profile-pi.vercel.app",
       image: "/assets/images/compro.gif",
       desc: "Web based Company Profile made with Typescript, Next.js, Chakra UI, Axios and React Slick.",
+      linkGithub: "https://github.com/farizwrmn/CompanyProfile",
     },
     {
       id: 5,
@@ -40,6 +45,7 @@ const Services = () => {
       link: "https://github.com/farizwrmn/invoeasy",
       image: "/assets/images/invoeasy.gif",
       desc: "Invoice management app for 2nd Final Project at Purwadhika Bootcamp made with Typescript, Next.js, Tailwind CSS, Nodemailer, Formik and Yup, Axios, MySQL, JWT, Express, and Prisma.",
+      linkGithub: "https://github.com/farizwrmn/invoeasy",
     },
     // {
     //   id: 6,
@@ -54,6 +60,7 @@ const Services = () => {
       link: "http://nutech-reactjs-redux.vercel.app/",
       image: "/assets/images/nutech.gif",
       desc: "This is a take home test for a Front End Developer gig at a software company in Indonesia. I made this app with Typescript, React, NextJS, Tailwind CSS, Redux and Axios",
+      linkGithub: "https://github.com/farizwrmn/nutech-reactjs-redux",
     },
   ];
 
@@ -78,8 +85,8 @@ const Services = () => {
       </div>
       <div>
         <ul>
-          {service.map(({ id, link, title, image, desc }) => (
-            <>
+          {service.map(({ id, link, title, image, desc, linkGithub }) => (
+            <React.Fragment key={id}>
               <li
                 key={id}
                 className="text-2xl text-center text-blue-950 py-10 font-bold mt-2 font-dirtyline"
@@ -94,6 +101,7 @@ const Services = () => {
                     width={500}
                     height={500}
                     className="rounded-xl"
+                    unoptimized
                   />
                 </Link>
               </div>
@@ -101,12 +109,14 @@ const Services = () => {
                 {desc}
                 <br />
                 <br />
-                (Click to visit)
+                <Link className="text-slate-50 mt-5 text-center text-sm p-2 tracking-tighter bg-blue-500 rounded-lg mr-4" href={link} target="_blank">Production</Link>
+                ●
+                <Link className="text-white mt-5 text-center text-sm p-2 tracking-tighter bg-black rounded-lg ml-4" href={linkGithub} target="_blank">Github</Link>
               </p>
               <h3 className="text-blue-950 mt-5 text-right">
                 ________________
               </h3>
-            </>
+            </React.Fragment>
           ))}
         </ul>
       </div>
